d4b73fa8d29204b9c0ea8cca48044a586b519770
EddieMachete <eduardo.er@gmail.com>
2014-04-16 10:05:44 -0700

    Added the Isncsci.Web sample project

---
+++ IsncsciWeb/Isncsci.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿/* Copyright 2014 Rick Hansen Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Isncsci.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Called to request the Asia Man SVG component of the ISNCSCI form
+        /// </summary>
+        /// <returns>SVG code required to render the Asia Man as inline svg</returns>
+        public ActionResult AsiaManSvg()
+        {
+            return View();
+        }
+    }
+}
---
+++ IsncsciWeb/Isncsci.Web/Scripts/Form/Index.js
@@ -0,0 +1,45 @@
+﻿/* Copyright 2014 Rick Hansen Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+var _loaded = false;
+var _ready = true;
+
+$(function () {
+    _loaded = true;
+    if (_ready) { Run(); }
+});
+
+function SciReady(e) {
+    _ready = true;
+    if (_loaded) { Run(); }
+}
+
+function Run() {
+    var controller = new sci.neurology.NeurologyTestController();
+    controller.Initialize($('body'));
+
+    var pageActions = $('.page-actions');
+
+    $(window).bind('scroll', function (e) {
+        if ($(window).scrollTop() >= 45) {
+            if (!pageActions.hasClass('fixed')) {
+                pageActions.addClass('fixed');
+            }
+        } else if (pageActions.hasClass('fixed')) {
+            pageActions.removeClass('fixed');
+        }
+
+        return true;
+    });
+}
